d401e48712391656204ca27ebc6e219e30d3e750
raffa-p <r.prota@studenti.unipi.it>
2025-12-17 17:33:55 +0100

    Initial Commit

---
+++ Cinemax/CinemaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaxApp.swift
+//  Cinemax
+//
+//  Created by Raffaele Prota on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CinemaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
