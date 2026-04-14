be72124a90f604f3315dbea1bf698bad62fbd673
raffa-p <r.prota@studenti.unipi.it>
2026-04-14 19:59:19 +0200

    __

---
+++ Cinemax/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  Cinemax
+//
+//
+
+struct config{
+    static let databaseUrl = "https://zggjpzfjrqmyoryulyoa.supabase.co"
+    static let databaseKey = "sb_publishable_HbYAxWMPiRKn2s41zXomug_PQeXyPQP"
+    static let apiKey = "6fe89e77cb55caf7e08917b72fdb1e43"
+    static let appVersion = "1.1.0"
+}
